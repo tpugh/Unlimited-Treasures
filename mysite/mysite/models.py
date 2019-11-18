0d7569e97534cdad51ca7142c176f20faedc81ff
Terrence Pugh <tpughnsu@gmail.com>
2019-11-17 23:05:55 -0500

    added directions for models file

--- mysite/mysite/models.py
+++ mysite/mysite/models.py
@@ -1,2 +1,12 @@
+"""
+http://www.learningaboutelectronics.com/Articles/How-to-create-a-database-table-in-Django.php
+1. add class
+2. python manage.py makemigrations [AppName]
+3. python manage.py migrate
+Example
+class Students(models.Model):
+    firstname= models.CharField(max_length=100)
+    lastname= models.CharField(max_length=100)
+    email= models.CharField(max_length=200)
+"""
 from django.db import models
-
