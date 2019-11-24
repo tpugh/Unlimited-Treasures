6a28031a35894eb50ffdef1dff9480a0b8adc106
Terrence Pugh <tpughnsu@gmail.com>
2019-11-24 18:40:41 -0500

    added all new models for views along with built in django methods

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Item, OrderItem, Order, Address, Payment, Coupon, Refund, UserProfile
+from django.conf import settings
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, View
+from django.shortcuts import redirect
+from django.utils import timezone
+from .forms import CheckoutForm, CouponForm, RefundForm, PaymentForm
+
 
 
 def index(request):
