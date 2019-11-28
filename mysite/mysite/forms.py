39a663de85f595108e5bbb5e9f9a9dd9b219a838
Terrence Pugh <tpughnsu@gmail.com>
2019-11-27 22:41:45 -0500

    added payment choice for credit demo payment

--- mysite/mysite/forms.py
+++ mysite/mysite/forms.py
@@ -13,8 +13,9 @@ from django_countries.widgets import CountrySelectWidget
 
 
 PAYMENT_CHOICES = (
-    ('S', 'Stripe'),
-    ('P', 'PayPal')
+    #('S', 'Stripe'),
+    #('P', 'PayPal')
+    ('C', 'Credit')
 )
 
 
