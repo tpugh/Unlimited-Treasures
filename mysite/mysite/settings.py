f8a8b27eb31b6a9caecc1364bac4621a5a3b4a61
DrValkaryon <57381290+DrValkaryon@users.noreply.github.com>
2019-12-01 19:30:58 -0500

    Revert "Revert "Bootstrap fixes""
    This reverts commit ce0bb6cc019bda583393b6bf09b9224fa138c87f.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':  'mysite',
         'USER': 'admin',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'Fallout4',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
